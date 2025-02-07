e0411885ef3e22a1880b4ffc14f140706474f4e7
Ben Sokol <git@bensokol.com>
2025-02-07 09:18:46 -0600

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
   name='slackwebhook',
-  version='1.3.7',
+  version='1.3.8',
   author='Ben Sokol',
   author_email='ben@bensokol.com',
   maintainer='Ben Sokol',
